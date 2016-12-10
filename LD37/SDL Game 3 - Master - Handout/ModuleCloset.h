ed7b69a635c0411ffbd6afed6cd07ba72f4a9e78
codeck1 <marc.mensajes@gmail.com>
2016-12-11 00:41:55 +0100

    Created ModuleCloset for the items

---
+++ LD37/SDL Game 3 - Master - Handout/ModuleCloset.h
@@ -0,0 +1,19 @@
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+
+
+class ModuleCloset : public Module
+{
+public:
+	ModuleCloset(Application* app, bool start_enabled = true);
+	~ModuleCloset();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public: 
+	SDL_Texture* background;
+	SDL_Texture* stars;
+};
