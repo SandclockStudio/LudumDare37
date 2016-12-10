cc8bd9093eff38eb7b6e02f30069723a47b4606d
David Cervera Martí <davidmarti.mensajes@gmail.com>
2016-12-10 22:56:02 +0100

    Creation ModuleSilk

---
+++ LD37/SDL Game 3 - Master - Handout/ModuleSilk.cpp
@@ -0,0 +1,12 @@
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "ModuleSilk.h"
+
+ModuleSilk::ModuleSilk(Application * app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSilk::~ModuleSilk()
+{
+}
