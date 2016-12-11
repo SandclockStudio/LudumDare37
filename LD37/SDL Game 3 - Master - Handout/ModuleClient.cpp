a6fed8b384f9f4075ab7f4bfed69038d43614708
David Cervera Martí <davidmarti.mensajes@gmail.com>
2016-12-11 22:04:13 +0100

    Fix delete clients

--- LD37/SDL Game 3 - Master - Handout/ModuleClient.cpp
+++ LD37/SDL Game 3 - Master - Handout/ModuleClient.cpp
@@ -58,10 +58,14 @@ update_status ModuleClient::Update()
 			}
 		}
 
-		if (tmp->data->pooped && tmp->data->handCleaned && tmp->data->position.x > SCREEN_WIDTH && tmp->data->position.y > SCREEN_HEIGHT)
+		if (tmp->data->pooped && tmp->data->handCleaned && tmp->data->position.x >= SCREEN_WIDTH-30 )
 		{
+
+			
+			tmp->data->collider->type = COLLIDER_NONE;
 			delete tmp->data;
 			active.del(tmp);
+
 			break;
 		}
 
@@ -199,7 +203,11 @@ bool Client::Update()
 		collider->rect = { position.x, position.y, r.w, r.h };
 
 	}
-
+	else
+	{
+		SDL_Rect r = current_animation->PeekCurrentFrame();
+		collider->rect = { 1000, position.y, r.w, r.h };
+	}
 
 	//Posicion de ejemplo
 
